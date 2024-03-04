3def2d872526f5da4deed69d1654abb00f27b2c4
KizikN <naemnik2.0@mail.ru>
2024-03-04 22:39:01 +0300

    а

--- z2.5.cpp
+++ z2.5.cpp
@@ -7,65 +7,73 @@
 
 using namespace std;
 
-void Get_in_Sort(char *L1, char *L2, int *Am, char *L, int n1, int n2, int& am);
+int n1 = 0, n2 = 0;
+
+struct Mas
+{
+	char* L = new char[n1 + n2];
+	int* Am = new int[n1 + n2];
+};
+
+void Get_in_Sort(Mas*& L1, Mas*& L2, Mas*& Ln, int n1, int n2);
 
 int main()
 {
-	int n1 = 0, am = 0;
+	
 	cout << "Enter a size of L1: ";
 	cin >> n1;
-	int n2 = 0;
 	cout << "Enter a size of L2: ";
 	cin >> n2;
-	char *L1 = new char[n1];
-	int *Am = new int[n1 + n2];
-	char* Ln = new char[n1 + n2];
-	char* L2 = new char[n2];
+	Mas *L1 = new Mas;
+	Mas* Ln = new Mas;
+	Mas* L2 = new Mas;
 
-	Get_in_Sort(L1, L2, Am, Ln, n1, n2, am);
+	Get_in_Sort(L1, L2, Ln, n1, n2);
 	
+	/*После сортировки, я решил представить наш конечный массив не в виде нового Mas,
+	а в виде изменённого Ln->Am (ссылки на первые встречи символов остаются, а ссылки на повторы изменяются на -1)*/
+
 	int aM = 0;
 	for (int i = 0; i < n1 + n2 - 1; i++)
 	{
 
-		if (Ln[aM] != Ln[Am[i]])
+		if (Ln->L[aM] != Ln->L[Ln->Am[i]])
 		{
-			Am[aM] = Am[i];
-			aM = Am[i];
+			Ln->Am[aM] = Ln->Am[i];
+			aM = Ln->Am[i];
 		}
 		else
-			Am[i] = -1;
+			Ln->Am[i] = -1;
 	}
-	Am[aM] = Am[n1 + n2 - 1];
-
+	Ln->Am[aM] = Ln->Am[n1 + n2 - 1];
 	aM = 0;
 	cout << endl;
-	while (Am[aM] != Am[n1 + n2 - 1])
+	while (Ln->Am[aM] != Ln->Am[n1 + n2 - 1])
 	{
-		cout << Ln[Am[aM]] << " ";
-		aM = Am[aM];
+		cout << Ln->L[Ln->Am[aM]] << " ";
+		aM = Ln->Am[aM];
 	}
-	cout << Ln[Am[aM]] << endl;
+	cout << Ln->L[Ln->Am[aM]] << endl;
 
 	return 0;
 }
 
-void Get_in_Sort(char* L1, char* L2, int* Am, char* Ln, int n1, int n2, int& am)
+void Get_in_Sort(Mas*& L1, Mas*& L2, Mas*& Ln, int n1, int n2)
 {
 	cout << "Enter L1:" << endl;
 	for (int i = 0; i < n1; i++)
 	{
-		cin >> L1[i];
-		Am[i] = i + 1;
+		cin >> L1->L[i];
+		Ln->Am[i] = i + 1;
 	}
 	cout << "Enter L2:" << endl;
 	for (int i = 0; i < n2; i++)
 	{
-		cin >> L2[i];
+		cin >> L2->L[i];
 		if (i == n2 - 1)
-			Am[i + n1] = 0;
+			Ln->Am[i + n1] = 0;
 		else
-			Am[i + n1] = i + 1 + n1;
+			Ln->Am[i + n1] = i + 1 + n1;
 	}
 	
 	//=============================
@@ -73,32 +81,28 @@ void Get_in_Sort(char* L1, char* L2, int* Am, char* Ln, int n1, int n2, int& am)
 	for (int i = 0; i < n1; i++)
 	{
 		if(i == 0)
-		Ln[i] = L1[i];
+		Ln->L[i] = L1->L[i];
 		else
 		{
 			int j = 0;
-			while (L1[i] < Ln[i - j - 1] && j != i)
+			while (L1->L[i] < Ln->L[i - j - 1] && j != i)
 			{
-				Ln[i - j] = Ln[i - j - 1];
+				Ln->L[i - j] = Ln->L[i - j - 1];
 				j++;
 			}
-				Ln[i - j] = L1[i];
+				Ln->L[i - j] = L1->L[i];
 		}
 	}
 
 	for (int i = n1; i < n1 + n2; i++ )
 	{
 		int j = 0;
-		while (L2[i - n1] < Ln[i - j - 1] && j != i)
+		while (L2->L[i - n1] < Ln->L[i - j - 1] && j != i)
 		{
-			Ln[i - j] = Ln[i - j - 1];
+			Ln->L[i - j] = Ln->L[i - j - 1];
 			j++;
 		}
-			Ln[i - j] = L2[i - n1];
+			Ln->L[i - j] = L2->L[i - n1];
 	}
-
-	for (int i = 0; i < n1 + n2 - 1; i++)
-		if (Ln[i] != Ln[i + 1])
-			am++;
 }
 
